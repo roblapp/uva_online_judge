6506362ba898d28270dcdc00c49ee065769da740
Rob Lapp <lapp.robert@yahoo.com>
2015-12-04 18:16:17 -0500

    update

--- UVASCRAP_CPP/main.cpp
+++ UVASCRAP_CPP/main.cpp
@@ -1,12 +1,31 @@
 #include <iostream>
+#include <vector>
+#include <map>
 #include <fstream>
 
 using namespace std;
 
 //#define read cin
+#define MAXN 100000
 
 int main() {
     ifstream read("test.txt");
+    int n, m, x, k;
+    map<int, int> mIndexer;
+    vector<int> g[MAXN];
+
+    while (read >> n >> m) {
+        k = 0;
+        for (int i = 0; i < n; i++) {
+            read >> x;
+            if (mIndexer.find(x) == mIndexer.end()) {
+                mIndexer[x] = k++;
+            } else {
+
+            }
+        }
+    }
+
 
 
     read.close();
